94eb92a67f3dd190165bd19640e3ed383379f7ff
Horace Pan <hopan@uchicago.edu>
2018-12-06 15:55:06 -0600

    add test script for cube string, add inverse moves

--- cube/str_cube.py
+++ cube/str_cube.py
@@ -29,6 +29,10 @@ def cycle_cc(s):
         s[1], s[3], s[0], s[2]
     )
 
+def make_cube_str(u, d, l, r, f, b):
+    return '{}{}{}{}{}{}'.format(u,d,l,r,f,b)
+
+# TODO: this is hardcoded for 2x2. Should make it generic for nxn
 def rot_u(cube_str):
     _f = get_face(cube_str, 'f')
     _l = get_face(cube_str, 'l')
@@ -41,7 +45,29 @@ def rot_u(cube_str):
     b_face =  _l[:2] + _b[2:]
     r_face =  _b[:2] + _r[2:]
     f_face =  _r[:2] + _f[2:]
-    return '{}{}{}{}{}{}'.format(
+    return make_cube_str(
+        u_face,
+        d_face,
+        l_face,
+        r_face,
+        f_face,
+        b_face
+    )
+
+def rot_iu(cube_str):
+    _f = get_face(cube_str, 'f')
+    _l = get_face(cube_str, 'l')
+    _b = get_face(cube_str, 'b')
+    _r = get_face(cube_str, 'r')
+
+    u_face = cycle_cc(get_face(cube_str, 'u'))
+    d_face = get_face(cube_str, 'd')
+    l_face =  _b[:2] + _l[2:] # b's top becomes l's top
+    b_face =  _r[:2] + _b[2:] # r's top becomes b's top
+    r_face =  _f[:2] + _r[2:] # f's top becomes r's top
+    f_face =  _l[:2] + _f[2:] # l's top becomes f's top
+
+    return make_cube_str(
         u_face,
         d_face,
         l_face,
@@ -62,7 +88,28 @@ def rot_d(cube_str):
     b_face =  _b[:2] + _l[2:]
     r_face =  _r[:2] + _b[2:]
     f_face =  _f[:2] + _r[2:]
-    return '{}{}{}{}{}{}'.format(
+    return make_cube_str(
+        u_face,
+        d_face,
+        l_face,
+        r_face,
+        f_face,
+        b_face
+    )
+
+def rot_id(cube_str):
+    _f = get_face(cube_str, 'f')
+    _l = get_face(cube_str, 'l')
+    _b = get_face(cube_str, 'b')
+    _r = get_face(cube_str, 'r')
+
+    u_face = get_face(cube_str, 'u')
+    d_face = cycle_cc(get_face(cube_str, 'd'))
+    l_face =  _l[:2] + _b[2:] # b's bot becomes l's bot
+    b_face =  _b[:2] + _r[2:] # r's bot becomes b's bot
+    r_face =  _r[:2] + _f[2:] # f's bot becomes r's bot
+    f_face =  _f[:2] + _l[2:] # l's bot becomes f's bot
+    return make_cube_str(
         u_face,
         d_face,
         l_face,
@@ -84,7 +131,29 @@ def rot_r(cube_str):
 
     r_face = cycle(get_face(cube_str, 'r'))
     l_face = get_face(cube_str, 'l')
-    return '{}{}{}{}{}{}'.format(
+    return make_cube_str(
+        u_face,
+        d_face,
+        l_face,
+        r_face,
+        f_face,
+        b_face
+    )
+
+def rot_ir(cube_str):
+    _f = get_face(cube_str, 'f')
+    _u = get_face(cube_str, 'u')
+    _b = get_face(cube_str, 'b')
+    _d = get_face(cube_str, 'd')
+
+    f_face = _f[0] + _u[1] + _f[2] + _u[3] # u's right becomes f's right
+    u_face = _u[0] + _b[2] + _u[2] + _b[0] # b's left(rev) becomes u's right
+    b_face = _d[3] + _b[1] + _d[1] + _b[3] # d's right(rev) becomes b's left
+    d_face = _d[0] + _f[1] + _d[2] + _f[3] # f's right(rev) becomes d's right
+
+    r_face = cycle_cc(get_face(cube_str, 'r'))
+    l_face = get_face(cube_str, 'l')
+    return make_cube_str(
         u_face,
         d_face,
         l_face,
@@ -106,7 +175,29 @@ def rot_l(cube_str):
 
     r_face = get_face(cube_str, 'r')
     l_face = cycle_cc(get_face(cube_str, 'l'))
-    return '{}{}{}{}{}{}'.format(
+    return make_cube_str(
+        u_face,
+        d_face,
+        l_face,
+        r_face,
+        f_face,
+        b_face
+    )
+
+def rot_il(cube_str):
+    _f = get_face(cube_str, 'f')
+    _u = get_face(cube_str, 'u')
+    _b = get_face(cube_str, 'b')
+    _d = get_face(cube_str, 'd')
+
+    u_face = _b[3] + _u[1] + _b[1] + _u[3] # b's right(rev) becomes u's left
+    f_face = _u[0] + _f[1] + _u[2] + _f[3] # u's left becomes f's left
+    d_face = _f[0] + _d[1] + _f[2] + _d[3] # f's left becomes d's left 
+    b_face = _b[0] + _d[2] + _b[2] + _d[0] # d's left(rev) becomes b's right
+
+    r_face = get_face(cube_str, 'r')
+    l_face = cycle(get_face(cube_str, 'l'))
+    return make_cube_str(
         u_face,
         d_face,
         l_face,
@@ -128,7 +219,29 @@ def rot_f(cube_str):
 
     f_face = cycle(get_face(cube_str, 'f'))
     b_face = get_face(cube_str, 'b')
-    return '{}{}{}{}{}{}'.format(
+    return make_cube_str(
+        u_face,
+        d_face,
+        l_face,
+        r_face,
+        f_face,
+        b_face
+    )
+
+def rot_if(cube_str):
+    _u = get_face(cube_str, 'u')
+    _r = get_face(cube_str, 'r')
+    _d = get_face(cube_str, 'd')
+    _l = get_face(cube_str, 'l')
+
+    u_face = _u[0] + _u[1] + _r[0] + _r[2] # r's left becomes u's bot
+    r_face = _d[1] + _r[1] + _d[0] + _r[3] # d's top(rev) becomes r's left
+    d_face = _l[1] + _l[3] + _d[2] + _d[3] # l's right becomes d's top 
+    l_face = _l[0] + _u[3] + _l[2] + _u[2] # u's bot(rev) becomes l's right
+
+    f_face = cycle_cc(get_face(cube_str, 'f'))
+    b_face = get_face(cube_str, 'b')
+    return make_cube_str(
         u_face,
         d_face,
         l_face,
@@ -150,7 +263,7 @@ def rot_b(cube_str):
 
     f_face = get_face(cube_str, 'f')
     b_face = cycle(get_face(cube_str, 'b'))
-    return '{}{}{}{}{}{}'.format(
+    return make_cube_str(
         u_face,
         d_face,
         l_face,
@@ -159,19 +272,25 @@ def rot_b(cube_str):
         b_face
     )
 
-
-'''
-# backwards rotations
-def rot_iu(cube_str):
-
-def rot_id(cube_str):
-
-def rot_ir(cube_str):
-
-def rot_il(cube_str):
-
-def rot_if(cube_str):
-
 def rot_ib(cube_str):
+    _u = get_face(cube_str, 'u')
+    _r = get_face(cube_str, 'r')
+    _d = get_face(cube_str, 'd')
+    _l = get_face(cube_str, 'l')
 
-'''
+    u_face = _l[2] + _l[0] + _u[2] + _u[3] # l's left(rev) becomes u's top
+    l_face = _d[2] + _l[1] + _d[3] + _l[3] # d's bot becomes l's left 
+    d_face = _d[0] + _d[1] + _r[3] + _r[1] # r's right becomes d's bot 
+    r_face = _r[0] + _u[0] + _r[2] + _u[1] # u's top becomes r's right 
+
+    f_face = get_face(cube_str, 'f')
+    b_face = cycle_cc(get_face(cube_str, 'b'))
+    return make_cube_str(
+        u_face,
+        d_face,
+        l_face,
+        r_face,
+        f_face,
+        b_face
+    )
+
---
+++ cube/test_cube.py
@@ -0,0 +1,67 @@
+import time
+import pdb
+from str_cube import *
+from cube import *
+
+def test():
+    cube = Cube(2)
+
+    cube.random_step(20)
+    faces = ['u', 'd', 'l', 'r', 'f', 'b']
+    faces = ['iu', 'id', 'ir', 'il', 'if', 'ib']
+    # faces: u, d, l, r, f, b
+    for f in faces:
+        for _ in range(10):
+            str_cube = cube.str_state()
+            if 'i' in f:
+                cube.inv_rotate(f[1])
+            rotated = rotate(str_cube, f)
+            if (rotated != cube.str_state()):
+                print('Incorrect for face: {}'.format(f))
+                cube.render()
+                print('====')
+                rotated_cube = Cube.from_str(rotated)
+                rotated_cube.render()
+                exit()
+             
+            cube.random_step(1)
+
+    print('All good!')
+
+def benchmark(k=1000):
+    '''
+    Compare cube manipulation using Cube object versus pure string cube implementation 
+    '''
+    cube = Cube(2)
+    cube.random_step(20)
+    str_cube = cube.str_state()
+
+    c_s = time.time()
+    for _ in range(k):
+        cube.inv_rotate('u')
+        cube.inv_rotate('d')
+        cube.inv_rotate('l')
+        cube.inv_rotate('r')
+        cube.inv_rotate('f')
+        cube.inv_rotate('b')
+
+    t_cube = time.time() - c_s
+
+    s_s = time.time()
+    for _ in range(k):
+        str_cube = rot_iu(str_cube)
+        str_cube = rot_id(str_cube)
+        str_cube = rot_il(str_cube)
+        str_cube = rot_ir(str_cube)
+        str_cube = rot_if(str_cube)
+        str_cube = rot_ib(str_cube)
+
+
+    t_s = time.time() - s_s
+    print('Computing {} face moves'.format(k))
+    print('Cube time: {:.2f}'.format(t_cube))
+    print(' Str time: {:.2f}'.format(t_s))
+
+if __name__ == '__main__':
+    test()
+    benchmark(1000)
