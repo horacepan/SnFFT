fix wreath product cyclic irrep bug. add functions to do block scaling for cyclic irreps

multi.py
@@ -1,16 +1,17 @@
 from tqdm import tqdm
 import ast
-import pickle
 import math
 import argparse
 import os
 import time
 import pdb
 import numpy as np
-from multiprocessing import Pool, Process
-from utils import tf, chunk
-from wreath import wreath_yor, cyclic_irreps
+from multiprocessing import Pool, Manager
+from utils import tf, chunk, check_memory, load_pkl
+from wreath import cyclic_irreps, block_cyclic_irreps, wreath_rep
 from young_tableau import wreath_dim
+from perm2 import sn
+from coset_utils import young_subgroup_perm, coset_reps
 
 TWO_CUBE_SIZE = 88179840
 def clean_line(line):
@@ -34,6 +35,14 @@ def mult_yor(irrep, scalar, save_dict):
         else:
             save_dict[k] += scalar * irrep[k]
 
+def mult_yor_block(irrep, scalar, block_scalars, save_dict):
+    for k in irrep.keys():
+        i, j = k
+        if k not in save_dict:
+            save_dict[k] = scalar * block_scalars[i] * irrep[k]
+        else:
+            save_dict[k] += scalar * block_scalars[i] * irrep[k]
+
 def convert_yor_matrix(irrep_dict, block_size, cosets):
     '''
     Alternative:
@@ -44,16 +53,16 @@ def convert_yor_matrix(irrep_dict, block_size, cosets):
     full_mat = np.zeros(shape, dtype=np.complex128)
 
     for tup_idx, mat in irrep_dict.items():
-        i, j = tup_idx 
+        i, j = tup_idx
         full_mat[block_size * i: block_size * (i+1), block_size * j : block_size * (j + 1)] = mat
 
-    return full_mat 
+    return full_mat
 
 def coset_size(alpha):
     alpha_size = math.factorial(alpha[0]) * math.factorial(alpha[1]) * math.factorial(alpha[2])
     return int(math.factorial(8) // alpha_size)
 
-def split_transform(fsplit_lst, irrep_dict, alpha, parts):
+def split_transform(fsplit_lst, irrep_dict, alpha, parts, mem_dict=None):
     '''
     fsplit_pkl: list of pkl file names of the distance values for a chunk of the total distance values
     irrep_dict: irrep dict 
@@ -61,32 +70,29 @@ def split_transform(fsplit_lst, irrep_dict, alpha, parts):
     parts: list/iterable of partitions of the parts of alpha
     '''
     print('     Computing transform on splits: {}'.format(fsplit_lst))
+    cos_reps = coset_reps(sn(8), young_subgroup_perm(alpha))
     save_dict = {}
     cyc_irrep_func = cyclic_irreps(alpha)
+    pid = os.getpid()
 
     for fsplit_pkl in fsplit_lst:
         with open(fsplit_pkl, 'r') as f:
             # dict of function values
             pkl_dict = load_pkl(fsplit_pkl)
-            for perm_tup, tup_dict in pkl_dict.items():
+            for perm_tup, tup_dict in tqdm(pkl_dict.items()):
                 for tup, dists in tup_dict.items():
                     dist_tot = sum(dists)
-                    # perm -> or_tup -> dists
-                    r_alpha = cyc_irrep_func(tup)
                     perm_rep = irrep_dict[perm_tup]  # perm_rep is a dict of (i, j) -> matrix
-                    mult_yor(perm_rep, dist_tot * r_alpha, save_dict)
-
+                    block_cyclic_rep = block_cyclic_irreps(tup, cos_reps, cyc_irrep_func)
+                    mult_yor_block(perm_rep, dist_tot, block_cyclic_rep, save_dict)
+            if mem_dict is not None:
+                mem_dict[pid] = max(check_memory(verbose=False), mem_dict.get(pid, 0))
             del pkl_dict
+
     block_size = wreath_dim(parts)
     n_cosets = coset_size(alpha)
     mat = convert_yor_matrix(save_dict, block_size, n_cosets)
     return mat
-
-def load_pkl(fname):
-    #print('loading from pkl: {}'.format(fname))
-    with open(fname, 'rb') as f:
-        res = pickle.load(f)
-        return res
 
 def full_transform(args, alpha, parts, split_chunks):
     print('Computing full transform for alpha: {} | parts: {}'.format(alpha, parts))
@@ -95,10 +101,11 @@ def full_transform(args, alpha, parts, split_chunks):
     print('Savename: {}'.format(savename))
     if os.path.exists(savename + '.npy'):
         print('Skipping. Already computed fourier matrix for: {} | {}'.format(alpha, parts))
-        exit()
+        #exit()
 
-    irrep_dir = os.path.join(args.pkldir, args.alpha)
+    manager = Manager()
     irrep_dict = load_pkl(os.path.join(args.pkldir, args.alpha, '{}.pkl'.format(parts)))
+    mem_usg = check_memory(verbose=False)
 
     if not os.path.exists(savedir_alpha):
         print('Making: {}'.format(savedir_alpha))
@@ -106,10 +113,11 @@ def full_transform(args, alpha, parts, split_chunks):
 
     if args.par > 1:
         print('Par process with {} processes...'.format(len(split_chunks)))
+        mem_dict = manager.dict()
         with Pool(len(split_chunks)) as p:
-            arg_tups = [(_fn, irrep_dict, alpha, parts) for _fn in split_chunks]
+            arg_tups = [(_fn, irrep_dict, alpha, parts, mem_dict) for _fn in split_chunks]
             matrices = p.starmap(split_transform, arg_tups)
-            np.save(savename, sum(matrices))
+            #np.save(savename, sum(matrices))
     else:
         print('Single thread...')
         matrices = []
@@ -117,11 +125,17 @@ def full_transform(args, alpha, parts, split_chunks):
         n_cosets = coset_size(alpha)
         shape = (block_size * n_cosets, block_size * n_cosets)
         result = np.zeros(shape, dtype=np.complex128)
-        for _fn in split_chunks[0]:
+        mem_dict = {}
+        for _fn in split_chunks:
             res = split_transform(_fn, irrep_dict, alpha, parts)
             matrices.append(res)
             result += res
-        np.save(savename, sum(matrices))
+        #np.save(savename, sum(matrices))
+
+    print('Post loading pickle mem usg: {:.4}mb | Final mem usg: {:.4f}mb'.format(mem_usg, check_memory(False)))
+    print('Processes')
+    for pid, usg in mem_dict.items():
+        print('{} | {:.4f}mb'.format(pid, usg))
     print('Done!')
 
 def main(args):
@@ -140,10 +154,6 @@ def main(args):
     #assert all(sum(parts[i]) == alpha[i] for i in range(len(parts))), 'Invalid partition for alpha!'
     full_transform(args, alpha, parts, split_chunks)
 
-def irrep_dir(alpha, prefix):
-    _dir = os.path.join(prefix, '{},{},{}'.format(*alpha))
-    return _dir
-
 def test_partial():
     start = time.time()
     irrep_dict = load_pkl('/local/hopan/cube/pickles/(0, 1, 7)/((), (1,), (4, 3)).pkl')
@@ -157,8 +167,8 @@ if __name__ == '__main__':
     parser.add_argument('--pkldir', type=str, default='/local/hopan/cube/pickles')
     parser.add_argument('--splitdir', type=str, default='/local/hopan/cube/split_or')
     parser.add_argument('--savedir', type=str, default='/local/hopan/cube/fourier')
-    parser.add_argument('--alpha', type=str, default='(0, 1, 7)')
-    parser.add_argument('--parts', type=str, default='((),(1,),(5,2))')
+    parser.add_argument('--alpha', type=str, default='(8, 0, 0)')
+    parser.add_argument('--parts', type=str, default='((7,1),(),())')
     parser.add_argument('--par', type=int, default=1, help='Amount of parallelism')
     args = parser.parse_args()
     tf(main, [args])

perm2.py
@@ -175,7 +175,7 @@ class Perm2:
  
 def sn(n, prefix='/local/hopan/'):
     # load mult table?
-    if n in SN_CACHE:
+    if n in SN_CACHE and len(SN_CACHE[n]) == np.math.factorial(n):
         return list(SN_CACHE[n].values())
 
     perm_tups = permutations(range(1, n+1))

test_wreath.py
@@ -7,12 +7,12 @@ from yor import yor, load_yor
 import unittest
 from wreath import young_subgroup_yor, wreath_yor, mult, get_mat, WreathCycSn, cyclic_irreps, wreath_rep
 from utils import load_irrep
+from coset_utils import young_subgroup_perm
 sys.path.append('./cube')
 from multi import *
 from str_cube import *
 
 class TestWreath(unittest.TestCase):
-    '''
     def test_wreath(self):
         alpha = (0, 3, 2)
         _parts = ((), (2,1), (1,1))
@@ -33,22 +33,27 @@ class TestWreath(unittest.TestCase):
         eye_g_mat = mult(eye, g, ydict)
         eye_mat = get_mat(eye * perm2.Perm2.from_tup(g), ydict)
         self.assertTrue(np.allclose(eye_mat, eye_g_mat))
-    '''
 
     def test_wreath_full(self):
         o1, p1 = get_wreath('YYRMRMWWRWRYWMYMGGGGBBBB') # 14
         o2, p2 = get_wreath('YYBWGYRWMRBWMRMGYBRBGGMW') # 3
+        o3, p3 = get_wreath('GGBWMGBBMRYGMRYRYBYRWWWM') # 4
         w1 = WreathCycSn.from_tup(o1, p1, order=3)
         w2 = WreathCycSn.from_tup(o2, p2, order=3)
+        w3 = WreathCycSn.from_tup(o3, p3, order=3)
 
-        prod = w1 * w2
-        o_prod = prod.cyc.cyc
-        p_prod = prod.perm.tup_rep
+        prod12 = w1 * w2
+        prod13 = w1 * w3
+        o12 = prod12.cyc.cyc
+        o13 = prod13.cyc.cyc
+        perm12 = prod12.perm.tup_rep
+        perm13 = prod13.perm.tup_rep
 
         # load some pickle
-        alpha = (5, 3, 0)
-        parts = ((3,2), (2,1), ())
-        cf = cyc_irrep_func = cyclic_irreps(alpha)
+        alpha = (2, 3, 3)
+        parts = ((1,1), (1,1,1), (2,1))
+        cos_reps = coset_reps(perm2.sn(8), young_subgroup_perm(alpha))
+        cyc_irrep_func = cyclic_irreps(alpha)
 
         start = time.time()
         print('Loading {} | {}'.format(alpha, parts))
@@ -57,11 +62,16 @@ class TestWreath(unittest.TestCase):
             exit()
         print('Done loading | {:.2f}s'.format(time.time() - start))
 
-        wreath1 = wreath_rep(o1, p1, yor_dict, cyc_irrep_func)
-        wreath2 = wreath_rep(o2, p2, yor_dict, cyc_irrep_func)
-        wreath_mult = wreath1.dot(wreath2)
-        wreath_dict = wreath_rep(o_prod, p_prod, yor_dict, cyc_irrep_func)
-        self.assertTrue(np.allclose(wreath_mult, wreath_dict))
+        wreath1 = wreath_rep(o1, p1, yor_dict, cos_reps, cyc_irrep_func, alpha)
+        wreath2 = wreath_rep(o2, p2, yor_dict, cos_reps, cyc_irrep_func, alpha)
+        wreath3 = wreath_rep(o3, p3, yor_dict, cos_reps, cyc_irrep_func, alpha)
+        w12 = wreath1.dot(wreath2)
+        w13 = wreath1.dot(wreath3)
+        wd12 = wreath_rep(o12, perm12, yor_dict, cos_reps, cyc_irrep_func, alpha)
+        wd13 = wreath_rep(o13, perm13, yor_dict, cos_reps, cyc_irrep_func, alpha)
+
+        self.assertTrue(np.allclose(w12, wd12))
+        self.assertTrue(np.allclose(w13, wd13))
 
 if __name__ == '__main__':
     unittest.main()

wreath.py
@@ -1,26 +1,53 @@
-from multiprocessing import Pool, Manager, Process
+from multiprocessing import Manager, Process
 import os
 import sys
-import random
 import time
 from functools import reduce
-from collections import Counter
-import itertools
 import pdb
-from young_tableau import FerrersDiagram
-from yor import yor, load_yor
+from yor import load_yor
 import numpy as np
-from utils import partitions, weak_partitions, check_memory, chunk
+from utils import check_memory, chunk
 import perm2
-from coset_utils import coset_reps, young_subgroup_canonical, young_subgroup_perm, young_subgroup, perm_from_young_tuple, tup_set
+from coset_utils import coset_reps, young_subgroup_perm, young_subgroup, tup_set
 sys.path.append('./cube')
 from str_cube import init_2cube, rotate, get_wreath
 
 def dot(perm, cyc):
+    '''
+    Have the permutation act on the cyclic irrep
+    perm: Perm2
+    cyc: tuple or CyclicGroup object (this works bc CyclicGroup object implemented __getitem__)
+    Returns: CyclicGroup o
+    '''
     p_inv = perm.inv()
     new_cyc = tuple(cyc[p_inv[i] - 1] for i in range(1, len(cyc) + 1))
     p_cyc = CyclicGroup(new_cyc, cyc.order)
     return p_cyc
+
+def dot_tup(perm, tup):
+    p_inv = perm.inv()
+    new_tup = tuple(tup[p_inv[i] - 1] for i in range(1, len(tup) + 1))
+    return new_tup
+
+def dot_tup_inv(perm, tup):
+    new_tup = tuple(tup[perm[i] - 1] for i in range(1, len(tup) + 1))
+    return new_tup
+
+def block_cyclic_irreps(tup, coset_reps, cyclic_irrep_func):
+    '''
+    tup: tuple
+    coset_reps: list of Perm2 objects
+    cyclic_irrep_func: function from tuple -> cyclic_irrep
+    Return a dictionary mapping coset rep index -> cyclic irrep
+    Ex:
+        coset_reps = [pi_0, pi_1, ...]
+        Returns: {0: cyclic_irrep_func(f \dot pi_0), 1: cyclic_irrep_func(f \dot pi_1), ...}
+    '''
+    scalars = {}
+    for idx, rep in enumerate(coset_reps):
+        tup_g = dot_tup_inv(rep, tup)
+        scalars[idx] = cyclic_irrep_func(tup_g)
+    return scalars
 
 class CyclicGroup:
     def __init__(self, cyc, order): 
@@ -83,28 +110,17 @@ def cyclic_irreps(weak_partition):
     Returns a function that takes in an 8 tuple and returns the
         product of the cyclic irreps (float)
     '''
-    _idx0 = weak_partition[0]
-    _idx1 = weak_partition[0] + weak_partition[1]
+    idx0 = weak_partition[0]
+    idx1 = weak_partition[0] + weak_partition[1]
     g_x = ((0,) * weak_partition[0]) + ((1,) * weak_partition[1]) + ((2,) * weak_partition[2])
-    def func(tup, perm):
-        # first indices 0: weak_partition[0]
-        # 2nd indices weak_partition[0]: weak_partition[0] + weak_partition[1]
-        # 3rd indices weak_partition[0] + weak_partition[1]:  sum(weak_partition)
-        ginv_perm = perm2.Perm2.from_tup(perm).inv()
-        tot = 0
-        for i in range(1, 9):
-            gidx = ginv_perm[i]-1 # perm is 1-indexed
-            gval = g_x[gidx]
-            tot += tup[i-1] * gval
-        return np.exp(2j * np.pi * tot / 3.)
-        p1 = sum(tup[:_idx0])
-        p2 = sum(tup[_idx0: _idx1])
-        p3 = sum(tup[_idx1:])
-
-        print('Evaluating on {} | {}, {}, {}'.format(tup, p1, p2, p3))
+    def func(tup):
+        p1 = sum(tup[:idx0])
+        p2 = sum(tup[idx0: idx1])
+        p3 = sum(tup[idx1:])
         return np.exp(2j * np.pi * 0 * p1 / 3.) * \
                np.exp(2j * np.pi * 1 * p2 / 3.) * \
                np.exp(2j * np.pi * 2 * p3 / 3.)
+
     return func
 
 def load_partition(partition, prefix='/local/hopan/irreps/'):
@@ -192,10 +208,10 @@ def _proc_yor(perms, young_yor, young_sub_set, reps, rep_dict):
         g_rep = {}
         for i, t_i in enumerate(reps):
             for j, t_j in enumerate(reps):
-                ti_g_tj = t_i.inv() * g * t_j
-                if ti_g_tj.tup_rep in young_sub_set:
-                    g_rep[(i, j)] = young_yor[ti_g_tj.tup_rep]
-
+                tiinv_g_tj = t_i.inv() * g * t_j
+                if tiinv_g_tj.tup_rep in young_sub_set:
+                    g_rep[(i, j)] = young_yor[tiinv_g_tj.tup_rep]
+                    break
         rep_dict[g.tup_rep] = g_rep 
 
 def wreath_yor_par(alpha, _parts, prefix='/local/hopan/', par=8):
@@ -216,9 +232,7 @@ def wreath_yor_par(alpha, _parts, prefix='/local/hopan/', par=8):
     young_sub_set = tup_set(young_sub)
     young_yor = young_subgroup_yor(alpha, _parts, os.path.join(prefix, 'irreps'))
     reps = coset_reps(_sn, young_sub)
-    #print('Len coset reps: {}'.format(len(reps)))
-    #print('Total loop iters: {}'.format(len(_sn) * len(reps) * len(reps)))
-    cnts = np.zeros((len(reps), len(reps)))
+
     sn_chunks = chunk(_sn, par)
     manager = Manager()
     rep_dict = manager.dict()
@@ -255,9 +269,6 @@ def wreath_yor(alpha, _parts, prefix='/local/hopan/'):
     young_yor = young_subgroup_yor(alpha, _parts, os.path.join(prefix, 'irreps'))
     reps = coset_reps(_sn, young_sub)
     rep_dict = {}
-    #print('Len coset reps: {}'.format(len(reps)))
-    #print('Total loop iters: {}'.format(len(_sn) * len(reps) * len(reps)))
-    cnts = np.zeros((len(reps), len(reps)))
 
     # this part can be parallelized
     # loop over the group
@@ -267,17 +278,16 @@ def wreath_yor(alpha, _parts, prefix='/local/hopan/'):
         g_rep = {}
         for i, t_i in enumerate(reps):
             for j, t_j in enumerate(reps):
-                ti_g_tj = t_i.inv() * g * t_j
-                if ti_g_tj.tup_rep in young_sub_set:
-                    g_rep[(i, j)] = young_yor[ti_g_tj.tup_rep]
-                    cnts[i, j] = cnts[i, j] + 1
+                tiinv_g_tj = t_i.inv() * g * t_j
+                if tiinv_g_tj.tup_rep in young_sub_set:
+                    g_rep[(i, j)] = young_yor[tiinv_g_tj.tup_rep]
+                    break
 
         rep_dict[g.tup_rep] = g_rep 
 
-    #return rep_dict, cnts
     return rep_dict
 
-def get_mat(g, yor_dict):
+def get_mat(g, yor_dict, block_scalars=None):
     '''
     g: perm2.Perm2 object
     yor_dict: dict mapping perm2.Perm2 object -> (dict of (i, j) -> numpy matrix)
@@ -291,22 +301,28 @@ def get_mat(g, yor_dict):
     vs = list(yg.values())
     block_size = vs[0].shape[0]
     size = len(yg) * block_size
-    mat = np.zeros((size, size))
+    mat = np.zeros((size, size), dtype=np.complex128)
 
     for (i, j), v in yg.items():
         x1, x2 = (block_size*i, block_size*i+block_size)
         y1, y2 = (block_size*j, block_size*j+block_size)
-        mat[x1:x2, y1:y2] = v
+        if block_scalars is not None:
+            scalar = block_scalars[i]
+        else:
+            scalar = 1
+        mat[x1:x2, y1:y2] = scalar * v
 
     return mat
 
-def wreath_rep(orientation, perm, yor_dict, cyc_irrep_func=None, alpha=None):
+def wreath_rep(orientation, perm, yor_dict, cos_reps, cyc_irrep_func=None, alpha=None):
     if (cyc_irrep_func is None) and (alpha is None):
+        pdb.set_trace()
         raise ValueError('Need to supply either irrep func or alpha')
     if cyc_irrep_func is None:
         cyc_irrep_func = cyclic_irreps(alpha)
 
-    return cyc_irrep_func(orientation, perm) * get_mat(perm, yor_dict)
+    block_scalars = block_cyclic_irreps(orientation, cos_reps, cyc_irrep_func)
+    return get_mat(perm, yor_dict, block_scalars)
 
 def mult(g, h, yd):
     '''
@@ -325,20 +341,14 @@ def mult(g, h, yd):
     return mat_g.dot(mat_h)
 
 def test_wreath_class():
-    perm = perm2.Perm2.from_tup((1,3,4,2))
-    cyc = CyclicGroup((0, 1, 0, 2), 3)
-    #w = WreathCycSn(cyc, perm)
     c = init_2cube()
     for f in ['r', 'l', 'f', 'b', 'u', 'd']:
         cube_str = rotate(c, f)
         o1, p1 = get_wreath(cube_str)
         o2, pinv = get_wreath(rotate(c, 'i' + f))
-        #c1 = CyclicGroup((2, 1, 0, 0, 0, 0, 2, 1), 3)
+
         c1 = CyclicGroup(o1, 3)
-        #c2 = CyclicGroup((2, 1, 0, 0, 0, 0, 2, 1), 3)
         c2 = CyclicGroup(o2, 3)
-        #p1 = perm2.Perm2.from_tup((2, 7, 3, 4, 5, 6, 8, 1))
-        #p2 = perm2.Perm2.from_tup((8, 1, 3, 4, 5, 6, 2, 7))
         p1 = perm2.Perm2.from_tup(p1)
         p2 = perm2.Perm2.from_tup(pinv)
 