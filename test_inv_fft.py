0b1ff5bbccbe3fdfa424368cf9dd841451d9e64d
Horace Pan <hopan@uchicago.edu>
2019-02-20 17:05:09 -0600

    add cleaner logging, removed deprecated bandlimiting in test_inv_fft

--- test_inv_fft.py
+++ test_inv_fft.py
@@ -1,7 +1,9 @@
 import sys
+import logging
 import pdb
 import random
 import time
+from datetime import datetime
 import argparse
 import os
 import numpy as np
@@ -21,15 +23,19 @@ SPLIT_SUBDIR = 'split_unmod'
 SYM_CUBES = 'cube_sym_mod.txt'
 DIST = 'dist'
 
+logger = logging.getLogger(__name__)
+print = logger.info
+
 def load_sym_cubes(prefixdir):
     all_cubes = pd.read_csv(os.path.join(prefixdir, SYM_CUBES), header=None)
     all_cubes.set_index(0, inplace=True)
     all_cubes.rename(columns={1: DIST}, inplace=True)
     return all_cubes
 
-def random_sample(n, maxdist, df):
+def random_sample(n, maxdist, mindist, df):
     # just load the cube_sym_mod
     df = df[df[DIST] <= maxdist]
+    df = df[df[DIST] >= mindist]
     sampled = random.sample(range(len(df)), n)
     cubes = df.iloc[sampled].index.values # cube strings are the index
     dists = df.iloc[sampled][DIST].values
@@ -52,19 +58,28 @@ def main():
     Loads the fourier and irrep dict and then computes the inverse fourier transform
     portion for this irrep.
     '''
-    print("Starting inverse fft test")
     parser = argparse.ArgumentParser(description='Test drive inverse fft')
     parser.add_argument('--prefix', type=str, default='/local/hopan/cube/')
     parser.add_argument('--samples', type=int, default=1)
-    parser.add_argument('--bandlimit', type=str, default='')
     parser.add_argument('--maxdist', type=int, default=15)
+    parser.add_argument('--mindist', type=int, default=0)
     parser.add_argument('--minnorm', type=float, default=0.)
+    parser.add_argument('--maxnorm', type=float, default=2.)
     parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--logfile', type=str, default='ift.log')
     args = parser.parse_args()
+
+    logging.basicConfig(
+         filename='{}_{}.log'.format(args.logfile,
+                                     datetime.now().strftime("%m_%d_%H_%M_%S")),
+         level=logging.INFO,
+         format= '[%(asctime)s][%(module)s][%(funcName)s]: %(message)s',
+         datefmt='%H:%M:%S'
+    )
+    print("Starting inverse fft test")
 
     random.seed(args.seed)
     np.random.seed(args.seed)
-
     start = time.time()
     peak_mem_usg = 0
     irreps_used = 0
@@ -72,9 +87,8 @@ def main():
     s8 = sn(8)
     ift_dists = {}
     true_dists = {}
-    bl_irreps = None
     df_cubes = load_sym_cubes(args.prefix)
-    cube_strs, _ = random_sample(args.samples, args.maxdist, df_cubes)
+    cube_strs, _ = random_sample(args.samples, args.maxdist, args.mindist, df_cubes)
 
     for c in cube_strs:
         for cnbr in neighbors_fixed_core(c):
@@ -82,27 +96,16 @@ def main():
             ift_dists[cnbr] = 0
             true_dists[cnbr] = df_cubes.loc[cnbr][DIST]
 
-    if args.bandlimit:
-        bl_func = eval(args.bandlimit)
-        bl_irreps = bl_func()
-        print('Using irreps: {}'.format(bl_irreps.keys()))
-
     for alpha in cube2_alphas():
-        if args.bandlimit and alpha not in bl_irreps:
-            continue
-
         cyc_irrep_func = cyclic_irreps(alpha)
         cos_reps = coset_reps(s8, young_subgroup_perm(alpha))
         for parts in partition_parts(alpha):
-            if args.bandlimit and parts not in bl_irreps[alpha]:
-                continue
-
             fourier_mat = load_fourier(args.prefix, alpha, parts)
             if fourier_mat is None:
                 continue
 
             mat_sn = np.linalg.norm(fourier_mat) * fourier_mat.shape[0] / (CUBE2_SIZE * 14.)
-            if mat_sn < args.minnorm:
+            if mat_sn < args.minnorm or mat_sn > args.maxnorm:
                 continue
 
             irreps = load_irrep(args.prefix, alpha, parts)
@@ -121,7 +124,7 @@ def main():
             irreps_used += 1
             used_irreps.append((alpha, parts))
             elapsed = (time.time() - start) / 60.
-            print('Elapsed: {:6.2f}min | Done with {} | {:30} | Fourier norm: {:.2f}'.format(elapsed, alpha, str(parts), mat_sn))
+            print('Done with {} | {:30} | Fourier norm: {:.2f}'.format(alpha, str(parts), mat_sn))
 
     correct_cubes = compute_correct_moves(ift_dists, true_dists, cube_strs)
     print('Used {} irreps:'.format(irreps_used))
@@ -152,7 +155,7 @@ def compute_correct_moves(ift_dists, true_dists, cube_states):
     n_correct = 0
     correct_cubes = []
     incorrect_cubes = []
-
+    base_rate = 0
     for cube in cube_states:
         nbrs = neighbors_fixed_core(cube)
         nbrs.sort(key=lambda x: ift_dists[x].real)
@@ -160,68 +163,16 @@ def compute_correct_moves(ift_dists, true_dists, cube_states):
         min_dist = min([true_dists[x] for x in nbrs])
         opt_nbrs = [x for x in nbrs if true_dists[x] == min_dist]
         opt_pol_nbr = nbrs[0]
+        base_rate += len(opt_nbrs) / len(nbrs)
         if opt_pol_nbr in opt_nbrs:
             n_correct += 1
             correct_cubes.append(cube)
         else:
             incorrect_cubes.append(cube)
-
+    base_rate = base_rate / len(cube_states)
     pp_diff(incorrect_cubes, ift_dists, true_dists, "incorrect")
+    print('Base rate for randomg guessing: {}'.format(base_rate))
     return correct_cubes
 
-def top():
-    irreps = {
-        (2, 3, 3): partition_parts((2, 3, 3)),
-        (8, 0, 0): [((8,), (), ())]
-    }
-    return irreps
-
-def top2():
-    irreps = {
-        (2, 3, 3): partition_parts((2, 3, 3)),
-        (4, 2, 2): partition_parts((4, 2, 2)),
-        (8, 0, 0): [((8,), (), ())]
-    }
-    return irreps
-
-def top4():
-    irreps = {
-        (2, 3, 3): partition_parts((2, 3, 3)),
-        (4, 2, 2): partition_parts((4, 2, 2)),
-        (3, 1, 4): partition_parts((3, 1, 4)),
-        (3, 4, 1): partition_parts((3, 4, 1)),
-        (8, 0, 0): [((8,), (), ())]
-    }
-    return irreps
-
-def t341():
-    irreps = {
-        (3, 4, 1): partition_parts((3, 4, 1)),
-        (8, 0, 0): [((8,), (), ())]
-    }
-    return irreps
-
-def t314():
-    irreps = {
-        (3, 1, 4): partition_parts((3, 1, 4)),
-        (8, 0, 0): [((8,), (), ())]
-    }
-    return irreps
-
-def big():
-    alphas = [
-        (2, 3, 3), (4, 2, 2), (3, 1, 4), (3, 4, 1), (0, 4, 4), (1, 2, 5), (1, 5, 2), (6, 1, 1)
-    ]
-    irreps = { a: partition_parts(a) for a in alphas }
-    irreps[(8, 0, 0)] = [((8,), (), ())]
-    return irreps
-
-def mode():
-    irreps = {
-        (8, 0, 0): [((8,), (), ())]
-    }
-    return irreps
-
 if __name__ == '__main__':
-    tf(main)
-    #tf(test_dists)
+    main()
