9b901823b9c31a4f29b47c8316a40257c8bd1afb
Horace Pan <hopan@uchicago.edu>
2019-04-02 14:36:57 -0500

    bug fix in complex sparse mat mul

--- complex_utils.py
+++ complex_utils.py
@@ -12,7 +12,7 @@ def cmm(t1r, t1i, t2r, t2i):
 def cmm_sparse(sparse_r, sparse_i, wr, wi):
     rr = torch.sparse.mm(sparse_r, wr)
     ri = torch.sparse.mm(sparse_r, wi)
-    ir = torch.sparse.mm(sparse_i, wi)
+    ir = torch.sparse.mm(sparse_i, wr)
     ii = torch.sparse.mm(sparse_i, wi)
     return (rr - ii, ri + ii)
 
@@ -21,13 +21,16 @@ def cmse(y_true, yr, yi):
     loss = (real_diff.pow(2) + yi.pow(2)).mean()
     return loss
 
-def cmse_real(ytr, yti, yr, yi):
+def cmse_min_imag(ytr, yti, yr, yi):
     '''
     The real parts should be close. The imaginary parts should also be close
     and they should be small.
     '''
     real_diff = ytr - yr
     im_diff = yti - yi
-    #loss = (real_diff.pow(3) + im_diff.pow(2) + yti.pow(2) + yi.pow(2)).mean()
-    loss = (real_diff.pow(2) + im_diff.pow(2)).mean()
+    loss = (real_diff.pow(2) + im_diff.pow(2) + yti.pow(2) + yi.pow(2)).mean()
     return loss
+
+def cmse_real_only(ytr, yti, yr, yi):
+    real_diff = ytr - yr
+    return real_diff.pow(2).mean()
