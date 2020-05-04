ac0bde23875555bf95fadaaca68ab9b5d1b316fe
Horace Pan <hopan@uchicago.edu>
2020-05-04 15:43:19 -0500

    astar logging

--- exp/astar.py
+++ exp/astar.py
@@ -19,12 +19,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 def a_star(state, done_state, perm_df, heuristic, max_explorable):
     to_visit = PriorityQueue()
     nodes_explored = 0
-    to_visit.put((0, 0, state, perm_df.distance(state)))
+    to_visit.put((0, 0, state))
     icnt = 0
     visited = set()
 
     while not to_visit.empty():
-        val, dist, curr_node, true_dist = to_visit.get()
+        val, dist, curr_node = to_visit.get()
         nodes_explored += 1
         if curr_node == done_state:
             return True, nodes_explored
@@ -36,7 +36,7 @@ def a_star(state, done_state, perm_df, heuristic, max_explorable):
                 continue
 
             fitness = heuristic(n) + dist
-            to_visit.put((fitness, dist + 1, n, perm_df.distance(n)))
+            to_visit.put((fitness, dist + 1, n))
 
         visited.add(curr_node)
         icnt += 1
@@ -85,68 +85,101 @@ def get_model(irreps, fn):
 
 def main(args):
     log = get_logger(None, stdout=True, tofile=False)
-    log.info('Saving in: {}'.format(args.saveloc))
-    res_fn = '/scratch/hopan/cube/irreps/cube2res/nres1_512_1024_25len/seed_1/model_270000.pt'
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    log.info('Saving in: {} | Seed: {}'.format(args.saveloc, args.seed))
+    #res_fn = '/scratch/hopan/cube/irreps/cube2res/nres1_512_1024_25len/seed_1/model_270000.pt'
+    res_fn =      '/scratch/hopan/cube/irreps/cube2res/nres1_1024_2048_ep25/seed_0/model_420000.pt'
+    res_best_fn = '/scratch/hopan/cube/irreps/cube2res/nres1_1024_2048_ep25/seed_0/model_490000.pt'
     irr1_fn = '/home/hopan//github/SnFFT/exp/logs/cube2test/233_2111111_100k/seed_0/model_{e}.pt'
     irr2_fn = '/home/hopan//github/SnFFT/exp/logs/cube2test/2111111_41111_200kcap_gpu_nodone/seed_1/model_80000.pt'
 
     to_wreath = lambda g: wreath_onehot(g, 3).to(device)
-    res_mlp = MLPResModel(88, 512, 1024, 1, nres=1, to_tensor=to_wreath)
+    res_mlp = MLPResModel(88, 1024, 2048, 1, nres=1, to_tensor=to_wreath)
+    res_best_mlp = MLPResModel(88, 1024, 2048, 1, nres=1, to_tensor=to_wreath)
     res_mlp.to(device)
+    res_best_mlp.to(device)
     res_mlp.load_state_dict(torch.load(res_fn, map_location=device))
+    res_best_mlp.load_state_dict(torch.load(res_best_fn, map_location=device))
     log.info('Loaded res mlp model')
 
     # load irreps
     irrep1 = [((2, 3, 3), ((2,), (1, 1, 1), (1, 1, 1)))]
     irrep2 = [((4, 2, 2), ((4,), (1, 1), (1, 1))), ((2, 3, 3), ((2,), (1, 1, 1), (1, 1, 1)))]
-    irrep1_net = get_model(irrep1, irr1_fn)
+    irr1_fn = '/scratch/hopan/cube/irreps/cube2test_irrep_sp/2111111_30len_20up_fixedcost_masked_p50_randommask/seed_2/model_30000.pt'
+    irr1_fn = '/scratch/hopan/cube/irreps/cube2irrep/irrep2_len25_done_mb32_random90/seed_4/model_20000.pt'
+    #irrep1_net = get_model(irrep1, irr1_fn)
+    #irrep1_net = get_model(irrep2, irr1_fn)
+    irrep1_net = get_model(irrep1 if 'irrep1' in irr1_fn else irrep2, irr1_fn)
+    log.info(f'Loaded 1 irrep models | {irr1_fn}')
+
+    '''
+    irrep2 = [((4, 2, 2), ((4,), (1, 1), (1, 1))), ((2, 3, 3), ((2,), (1, 1, 1), (1, 1, 1)))]
     log.info('Loaded 1 irrep models')
     irrep2_net = get_model(irrep2, irr2_fn)
     log.info('Loaded 2 irrep models')
+    '''
 
     prefix = 'scratch' if os.path.exists('/scratch/hopan/cube/cube_sym_mod_tup.txt') else 'local'
     perm_df = WreathDF(f'/{prefix}/hopan/cube/cube_sym_mod_tup.txt', 6, 3, f'/{prefix}/hopan/cube/cube_sym_mod_tup.pkl')
     log.info('Loaded perm df')
     heuristic = gen_mlp_model(res_mlp)
+    heuristic_best = gen_mlp_model(res_best_mlp)
     irr1_heuristic = gen_irrep_heuristic(irrep1_net)
+    '''
     irr2_heuristic = gen_irrep_heuristic(irrep2_net)
+    '''
 
     cnt = args.cnt
     max_explorable = args.max_exp
     _states = [perm_df.random_state(1000 + (1 if random.random() < 0.5 else 0)) for _ in range(cnt)]
 
+    '''
     st = time.time()
     log.info('Starting 2 irreps...')
     solve_status_2, _, explored2 = test_heuristic(perm_df, irr2_heuristic, _states, max_explorable=100)
-    log.info('2 irrep | Solves: {:.3f} | Avg: {:.2f} | Median: {:.2f} | Elapsed: {:.2f}mins'.format(sum(solve_status_2) / len(_states), np.mean(explored2),
-             np.median(explored2), (time.time()-st) / 60))
-
+    log.info('2 irrep | Solves: {:.3f} | Avg: {:.2f} | Median: {:.2f} | Max: {} | Elapsed: {:.2f}mins'.format(sum(solve_status_2) / len(_states), np.mean(explored2),
+             np.median(explored2), np.max(explored2), (time.time()-st) / 60))
+    '''
     st = time.time()
     log.info('Starting 1 irreps...')
-    solve_status_1, _, explored1 = test_heuristic(perm_df, irr1_heuristic, _states, max_explorable=100)
-    log.info('1 irrep | Solves: {:.3f} | Avg: {:.2f} | Median: {:.2f} | Elapsed: {:.2f}mins'.format(sum(solve_status_1) / len(_states), np.mean(explored1),
-             np.median(explored1), (time.time()-st) / 60))
+    solve_status_1, _, explored1 = test_heuristic(perm_df, irr1_heuristic, _states, max_explorable=max_explorable)
+    log.info('1 irrep | Solves: {:.3f} | Avg: {:.2f} | Median: {:.2f} | Max: {} | Elapsed: {:.2f}mins'.format(sum(solve_status_1) / len(_states), np.mean(explored1),
+             np.median(explored1), np.max(explored1), (time.time()-st) / 60))
+
+    st = time.time()
+    true_solves_best = []
+    log.info('Starting res41 with max exp budget = {} | cnt = {} | {} ...'.format(max_explorable, cnt, res_best_fn))
+    solve_status_best, _states_best, explored_best = test_heuristic(perm_df, heuristic_best, _states, max_explorable=max_explorable)
+    for _s, _exp in zip(solve_status_best, explored_best):
+        if _s:
+            true_solves_best.append(_exp)
+    log.info('Res net | Solves: {:.3f} | Avg: {:.2f} | Median: {:.2f} | Max: {} | Elapsed: {:.2f}mins'.format(sum(solve_status_best) / len(_states),
+             np.mean(true_solves_best), np.median(true_solves_best), np.max(true_solves_best), (time.time() - st) / 60))
 
     st = time.time()
     true_solves = []
-    log.info('Starting res with max exp budget = {} | cnt = {} ...'.format(max_explorable, cnt))
+    log.info('Starting res42  with max exp budget = {} | cnt = {} | {} ...'.format(max_explorable, cnt, res_fn))
     solve_status_res, _states, explored = test_heuristic(perm_df, heuristic, _states, max_explorable=max_explorable)
     for _s, _exp in zip(solve_status_res, explored):
         if _s:
             true_solves.append(_exp)
-    log.info('Res net | Solves: {:.3f} | Avg: {:.2f} | Median: {:.2f} | Elapsed: {:.2f}mins'.format(sum(solve_status_res) / len(_states),
-             np.mean(true_solves), np.median(true_solves), (time.time() - st) / 60))
+    log.info('Res net | Solves: {:.3f} | Avg: {:.2f} | Median: {:.2f} | Max: {} | Elapsed: {:.2f}mins'.format(sum(solve_status_res) / len(_states),
+             np.mean(true_solves), np.median(true_solves), np.max(true_solves), (time.time() - st) / 60))
 
     results = {
         'states': _states,
         'irrep1': {'solved': solve_status_1, 'explored': explored1, 'model': irr1_fn},
         'irrep2': {'solved': solve_status_2, 'explored': explored2, 'model': irr2_fn},
-        'res_model': {'solved': solve_status_res, 'explored': explored, 'model': res_fn},
+        'res_model': {'solved': solve_status_best, 'explored': explored_best, 'model': res_best_fn},
     }
     json.dump(results, open(args.saveloc, 'w'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--cnt', type=int, default=100)
     parser.add_argument('--max_exp', type=int, default=100)
     parser.add_argument('--saveloc', type=str, default=f'/scratch/hopan/cube/eval_logs/{time.time()}.json')
--- exp/cube3_rl.py
+++ exp/cube3_rl.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 import sys
 import os
+import time
 import json
 sys.path.append('/home/hopan/github/SnFFT/exp/')
 
@@ -20,6 +21,9 @@ from rlmodels import MLPResModel, DVN
 from utility import ReplayBuffer, update_params, check_memory
 import pdb
 
+from astar import a_star, gen_mlp_model
+from cube_main import try_load_weights
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def set_seed(seed):
@@ -27,7 +31,7 @@ def set_seed(seed):
     np.random.seed(seed)
     random.seed(seed)
 
-def can_solve(model, env, state, max_steps):
+def can_solve(model, env, state, max_steps, argmin=True):
     if env.is_done(state):
         return True
 
@@ -36,10 +40,29 @@ def can_solve(model, env, state, max_steps):
         nbr_vals = model.forward(env.to_tensor(nbrs)).detach()
         if any(env.is_done(n) for n in nbrs):
             return True
-        best_action = nbr_vals.argmin().item()
+        if argmin:
+            best_action = nbr_vals.argmin().item()
+        else:
+            best_action = nbr_vals.argmax().item()
         state = env.step(state, best_action)
 
     return False
+
+def astar_bench(policy, env, trials, max_exp, scramble_len):
+    correct = 0
+    solve_lens = []
+
+    done_state = env.start_state()
+    hfunc = lambda s: policy.forward(env.to_tensor([s])).item()
+
+    for _ in range(trials):
+        start = env.random_state(scramble_len + (1 if random.random() < 0.5 else 0))
+        corr, num = a_star(start, done_state, env, hfunc, max_exp)
+        if corr:
+            solve_lens.append(num)
+            correct += 1
+
+    return correct, solve_lens
 
 def benchmark(model, env, trials, max_steps, scramble_len=1000):
     correct = 0
@@ -49,18 +72,28 @@ def benchmark(model, env, trials, max_steps, scramble_len=1000):
         correct += can_solve(model, env, state, max_steps)
     return correct
 
+def unique_logger(sumdir):
+    cnt = 0
+    logfile = os.path.join(sumdir, f'output.log')
+    while os.path.exists(logfile):
+        logfile = os.path.join(sumdir, f'output{cnt}.log')
+        cnt += 1
+    return logfile
+
 def main(args):
     set_seed(args.seed)
     sumdir = os.path.join(f'{args.savedir}', f'{args.env}', f'{args.notes}/seed_{args.seed}')
-    if not os.path.exists(sumdir) and args.savelog:
-        os.makedirs(sumdir)
-        log = get_logger(fname=os.path.join(sumdir, 'output.log'), stdout=True, tofile=True)
-        log.info('Saving in: {}'.format(sumdir))
-        # save the json too
+    if args.savelog:
+        try:
+            os.makedirs(sumdir)
+        except:
+            pass
+        logfile = unique_logger(sumdir)
+        log = get_logger(fname=logfile, stdout=True, tofile=True)
         json.dump(args.__dict__, open(os.path.join(sumdir, 'args.json'), 'w'), indent=2)
+        log.info("Saving in: {} | Starting ...".format(logfile))
     else:
         log = get_logger(fname=None, stdout=True, tofile=False)
-    log.info("Starting ...")
 
     if args.env == 'Cube3':
         env = Cube3()
@@ -73,6 +106,9 @@ def main(args):
     nactions = len(env.moves)
     nin = env.to_tensor([env.start_state()]).numel()
     nout = 1
+    updates = 0
+    icnt = 0
+    start_ep = 0
 
     replay = ReplayBuffer(nin, args.capacity)
     policy = MLPResModel(nin, args.resfc1, args.resfc2, nout, args.nres, to_tensor=env.to_tensor, std=args.std)
@@ -84,12 +120,15 @@ def main(args):
         policy.xinit()
         target.xinit()
 
+    if args.loadmodel:
+        loaded, start_ep = try_load_weights(sumdir, policy, target)
+        if loaded:
+            log.info(f'Loaded models! | Starting at epoch: {start_ep}')
+
     optim = torch.optim.Adam(policy.parameters(), lr=args.lr)
     log.info("Done setup ...")
 
-    updates = 0
-    icnt = 0
-    for e in range(args.epochs):
+    for e in range(start_ep, start_ep + args.epochs + 1):
         states = env.random_walk(args.rw_length)
         for state in states:
             action = random.randint(0, len(env.moves) - 1)
@@ -116,8 +155,15 @@ def main(args):
             updates += 1
 
         if e % args.logint == 0:
-            correct = benchmark(policy, env, trials=args.trials, max_steps=args.max_steps, scramble_len=args.scramble_len)
-            log.info(f'Epoch: {e:4d} | Solves: {correct/args.trials:.3f} | Updates: {updates:4d} | Iters: {icnt:6d} | Mem: {check_memory(verbose=False):.2f}mb')
+            #correct = benchmark(policy, env, trials=args.trials, max_steps=args.max_steps, scramble_len=args.scramble_len)
+            _astart = time.time()
+            correct, solves_lens = astar_bench(policy, env, trials=args.trials, max_exp=args.max_exp, scramble_len=args.scramble_len)
+            _atime = (time.time() - _astart) / 60.
+            avg = 0
+            if correct > 0:
+                avg = np.mean(solves_lens)
+
+            log.info(f'Epoch: {e:4d} | A* Solves: {correct/args.trials:.2f} | Avg len: {avg:.2f} | Updates: {updates:4d} | Iters: {icnt:6d} | Mem: {check_memory(verbose=False):.2f}mb | A* time: {_atime:.2f}mins')
 
             torch.save(policy.state_dict(), os.path.join(sumdir, f'model_last.pt'))
 
@@ -130,6 +176,7 @@ if __name__ == '__main__':
     parser.add_argument('--env', type=str, default='Cube3')
     parser.add_argument('--savedir', type=str, default='/scratch/hopan/cube/irreps/')
     parser.add_argument('--savelog', action='store_true', default=False)
+    parser.add_argument('--loadmodel', action='store_true', default=False)
     parser.add_argument('--notes', type=str, default='test')
 
     # hyperparams
@@ -144,9 +191,10 @@ if __name__ == '__main__':
     parser.add_argument('--target_update', type=int, default=20)
     parser.add_argument('--update_int', type=int, default=50)
     parser.add_argument('--trials', type=int, default=100)
+    parser.add_argument('--max_exp', type=int, default=100)
     parser.add_argument('--rw_length', type=int, default=10)
     parser.add_argument('--max_steps', type=int, default=10)
-    parser.add_argument('--scramble_len', type=int, default=10)
+    parser.add_argument('--scramble_len', type=int, default=20)
 
     # model params
     parser.add_argument('--std', type=float, default=0.01)
